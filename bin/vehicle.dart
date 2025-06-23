c6a4fb89b24e3c35ee376e84b07c77f1313f895c
RohanKhan758 <134365711+RohanKhan758@users.noreply.github.com>
2025-06-23 19:06:21 +0600

    Initial commit

---
+++ bin/vehicle.dart
@@ -0,0 +1,33 @@
+abstract class Vehicle{
+  int _speed = 0;
+      
+  void move();  
+  
+  int get speed => _speed;
+  
+  setSpeed(int speed){
+    if(speed >0){
+      _speed = speed;
+      print('The current speed of the car is $speed k.m');
+    }else if(speed == 0) {
+      _speed = speed;
+      print('Speed of the car is $speed, the car is now standing still.');
+    }else{
+      print('Car speed can\'t be negative');
+    }
+  }
+}
+
+class Car extends Vehicle{
+ 
+  @override
+  move(){
+    print('The car is moving at $_speed km/h');
+  }
+}
+
+void main(){
+  Car car = Car();
+  car.setSpeed(0);
+  car.move();
+}
